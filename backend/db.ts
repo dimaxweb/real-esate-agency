1d3235eaa2d2048b1fa9804478749278cb90f5e7
Arthur Blinov <blinov.arthur2020@gmail.com>
2024-12-18 09:26:14 +0200

    fb authentification code added

--- backend/db.ts
+++ backend/db.ts
@@ -11,4 +11,12 @@ const pool = new Pool({
   port: 8001,
 });
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'realtorboost',
+//   password: '@123Web',
+//   port: 5432,
+// });
+export const query = (text: string, params: any[]) => pool.query(text, params);
 export default pool;
